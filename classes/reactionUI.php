256750f1acb38a55f567d983377c7045e0deb1f0
t6767@mail.ru <t6767@mail.ru>
2019-12-09 16:39:10 +0600

    up

--- classes/reactionUI.php
+++ classes/reactionUI.php
@@ -6,12 +6,14 @@ class reactionUI extends PDO
 	public $idmsg;
 	public $dt;
 	public $ms;
+	public $access;
 
 	public function __construct($file = 'my_setting.ini')
     {
         if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable11 to open ' . $file . '.');
         $dns = $settings['database']['driver'].':host=' . $settings['database']['host'].((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '').';dbname='.$settings['database']['schema'];
         parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
+        $this->access=0;
     }
 
 	public function inital($unm, $uid, $idmsg, $dt, $ms)
@@ -21,6 +23,7 @@ class reactionUI extends PDO
 		$this->idmsg=$idmsg;
 		$this->dt=$dt;
 		$this->ms=$ms;
+		if ($this->user=='Тимур') $this->access=1;
 	}
 	
 	function saveToBase($res)
@@ -32,14 +35,16 @@ class reactionUI extends PDO
 	
 		function saveMessage()
     {
-		$id_message=$this->idmsg;
-		$id_user=$this->iduser;
-		$uname=$this->user;
-		$date=$this->dt;
-		$msg=$this->ms;
-        $sql = "INSERT INTO lids (id_message, id_user, uname, date, msg) VALUES ($id_message, '$id_user', '$uname', '$date', '$msg')";
-        $query = $this->prepare($sql);
-        $query->execute();
+        if ($this->access==1) {
+            $id_message = $this->idmsg;
+            $id_user = $this->iduser;
+            $uname = $this->user;
+            $date = $this->dt;
+            $msg = $this->ms;
+            $sql = "INSERT INTO lids (id_message, id_user, uname, date, msg) VALUES ($id_message, '$id_user', '$uname', '$date', '$msg')";
+            $query = $this->prepare($sql);
+            $query->execute();
+        }
     }
 	
 	
