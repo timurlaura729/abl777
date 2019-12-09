b0839ef93486bf240cae47a1e858187caec35271
t6767@mail.ru <t6767@mail.ru>
2019-12-09 16:52:14 +0600

    +telegramClass

--- classes/reactionUI.php
+++ classes/reactionUI.php
@@ -7,6 +7,7 @@ class reactionUI extends PDO
 	public $dt;
 	public $ms;
 	public $access;
+	public $url;
 
 	public function __construct($file = 'my_setting.ini')
     {
@@ -14,6 +15,7 @@ class reactionUI extends PDO
         $dns = $settings['database']['driver'].':host=' . $settings['database']['host'].((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '').';dbname='.$settings['database']['schema'];
         parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
         $this->access=0;
+        $this->url='https://api.telegram.org/bot1059041833:AAHi7sjrHjDh97eWhF266jTvlkua3glSJ90/sendMessage';
     }
 
 	public function inital($unm, $uid, $idmsg, $dt, $ms)
@@ -44,10 +46,43 @@ class reactionUI extends PDO
             $sql = "INSERT INTO lids (id_message, id_user, uname, date, msg) VALUES ($id_message, '$id_user', '$uname', '$date', '$msg')";
             $query = $this->prepare($sql);
             $query->execute();
+           $this->sendMessage('Здравствуйте',$buttons = null);
         }
     }
-	
-	
+
+    function sendMessage($message,$buttons = null) {
+
+        $data = array(
+            'text' => $message,
+            'chat_id' => $this->iduser
+        );
+
+        if($buttons != null) {
+            $data['reply_markup'] = [
+                'keyboard' => $buttons,
+                'resize_keyboard' => true,
+                'one_time_keyboard' => true,
+                'parse_mode' => 'HTML',
+                'selective' => true
+            ];
+        } else {
+        }
+
+        $data_string = json_encode($data);
+
+        $ch = curl_init($this->url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+        );
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
 
 }
 ?>
