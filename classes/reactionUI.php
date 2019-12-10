1bba36b613d4c34eca889bfbc7e2f31b13977569
t6767@mail.ru <t6767@mail.ru>
2019-12-10 12:08:28 +0600

    up

--- classes/reactionUI.php
+++ classes/reactionUI.php
@@ -1,4 +1,43 @@
 <?php
+class planFX
+{
+    public $api_server = 'https://api.planfix.ru/xml/';
+    public $api_key = '6c7c7be26c25aaa7b67a0d538d899f1c';
+    public $api_secret = '523e109934f51b6af56e0433074cc8cb';
+    public $api_token = '551804bdcdab76da99bb570834620545';
+
+    public function __construct()
+    {
+    }
+
+    public function getUIK($uik)
+    {
+        //QWEB199
+        $requestXml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><request method="contact.getList"><filters><filter><type>4101</type><field>14926</field><operator>equal</operator><value>'.$uik.'</value></filter></filters></request>');
+        $requestXml->account = 'amanbolkz';
+        $requestXml->pageCurrent = 1;
+        $ch = curl_init($this->api_server);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // не выводи ответ на stdout
+        curl_setopt($ch, CURLOPT_HEADER, 1);   // получаем заголовки
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // не проверять SSL сертификат
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // не проверять Host SSL сертификата
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->api_key . ':' . $this->api_token);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $requestXml->asXML());
+        $response = curl_exec($ch);
+        $error = curl_error($ch);
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $responseBody = substr($response, $header_size);
+        curl_close($ch);
+        $temp = trim($responseBody);
+        $xml = simplexml_load_string($temp);
+        $json = json_encode($xml);
+        $array = json_decode($json, TRUE);
+        return $array;
+    }
+}
+
 class reactionUI extends PDO
 {
     // Глобальные переменные
@@ -10,6 +49,7 @@ class reactionUI extends PDO
 	public $access;
 	public $url;
 	public $maenu1;
+	public $planFX;
 
 	// Создадим конструктор ебаный Лего
 	public function __construct($unm, $uid, $idmsg, $dt, $ms, $file = 'my_setting.ini')
@@ -28,6 +68,7 @@ class reactionUI extends PDO
         $this->idmsg=$idmsg;
         $this->dt=$dt;
         $this->ms=$ms;
+        $this->planFX=new planFX();
         // Проверка на пользователя. Оставь надежду всяк сюда входящий
         if ($this->ms=='777') $this->access=1;
     }
@@ -101,6 +142,8 @@ class reactionUI extends PDO
                 if ($this->access==1) {
                     $this->saveLog();
                     $this->saveAuth();
+                    $ms=print_r($this->planFX->getUIK('QWEB199'), true);
+                    $this->saveToBase($ms."777777");
                     $this->sendMessage($this->user . ' введите УИК ', $buttons = null);
                 } else $this->sendMessage($this->user . ' бот не слушает ваши команды. Авторизуйтесь и выполните их снова', $buttons = null);
             }
